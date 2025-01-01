5bd3704970977e11a4881dd9366910acf75e1ded
Robson Tenório <rrtenorio@gmail.com>
2025-01-01 10:28:22 -0300

    WIP

--- databases/sqlserver.sh
+++ databases/sqlserver.sh
@@ -7,8 +7,8 @@ set -e
 
 apt install -y php8.4-dev unixodbc-dev
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
+curl -k https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /usr/share/keyrings/microsoft-prod.gpg
+curl -k https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 apt update
 ACCEPT_EULA=Y apt-get install -y msodbcsql18 mssql-tools18
 
